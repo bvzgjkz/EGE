b24c8f58a78dc614ef9b4a354e268492817296f7
dgscene <mermal2000@gmail.com>
2024-02-27 12:20:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main() {
+	double x, y;
+	printf("Введите значение: ");
+	scanf_s("%lf", &x);
+	y = exp(x);
+	printf("e ^ %lf = %lf\n", x, y);
+	return 0;
+}
